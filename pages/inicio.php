f60c9968faf331b71c22f0af23ce9d2836658f1b
rafaelnargolo <124110743+rafaelnargolo@users.noreply.github.com>
2024-02-07 13:11:16 -0300

    Exibir anotação após clicar

--- pages/inicio.php
+++ pages/inicio.php
@@ -28,119 +28,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style.css">
     <title>Início</title>
-    <style>
-        #ola {
-            text-align: center;
-            margin: 40px;
-        }
-
-        #centro {
-            display: flex;
-            justify-content: flex-start;
-            align-items: center ;
-            flex-direction: column;
-            margin: auto;
-            min-height: 500px;
-            width: 400px;
-            background-color: cadetblue;
-            border-radius: 10px;
-            box-shadow: 2px 2px 5px rgba(0, 0, 0, .643);
-        }
-
-        button#adicionar {
-            display: block;
-            width: 60px;
-            height: 60px;
-            text-decoration: none;
-            margin: auto;
-            margin-bottom: 45px;
-            line-height: 62px;
-            color: gray;
-            text-align: center;
-            font-size: 40px;
-            background-color: antiquewhite;
-            border: none;
-            border-radius: 50%;
-            transition: 0.2s;
-            
-        }
-
-        button#adicionar:hover {
-            color: greenyellow;
-            transform: scale(120%);
-            cursor: pointer;
-        }
-
-        #escrever-anotacao {
-            position: fixed;
-            display: none;
-            align-items: center;
-            justify-content: center;
-            top: 0;
-            left: 0;
-            width: 100vw;
-            height: 100vh;
-            opacity: 0.5;
-            background-color: black;
-        }
-
-        form {
-            display: none;
-            position: absolute;
-            left: 50%;
-            top: 50%;
-            transform: translate(-50%, -50%);
-            background-color: white;
-            border-radius: 10px;
-            width: 400px;
-            height: 500px;
-            z-index: 1;
-        }
-
-        #ita {
-            background-color: #BDB9B9;
-            border-radius: 10px;
-            padding: 10px;
-            width: 320px;
-            resize: none;
-            border: none;
-            box-shadow: 3px 3px 5px rgba(0, 0, 0, .6);
-        }
-
-        #mostrar-anotacao {
-            display: flex;
-            align-items: center;
-            justify-content: space-around;
-            width: 300px;
-            height: 80px;
-            background-color: aliceblue;
-            margin-top: 20px;
-            border-radius: 5px;
-        }
-
-        #mostrar-anotacao > h3 {
-            font-size: 20px;
-            margin-left: -40px;
-            padding-left: 5px;
-            border-left: 5px solid greenyellow;
-
-        }
-
-        #mostrar-anotacao > button {
-            margin-right: -40px;
-            border: none;
-            cursor: pointer;
-            padding: 5px;
-            background-color: whitesmoke;
-            border-radius: 5px;
-            transition: 0.2s;
-        }
-
-        #mostrar-anotacao > button:hover {
-            background-color: red;
-        }
-
-    </style>
+    <link rel="stylesheet" href="style.css">
 
 </head>
 <body>
@@ -156,6 +44,10 @@
             Suas anotações
         </p>
 
+        <button id="adicionar" onclick="popup()" title="Adicionar anotação">
+            +
+        </button>
+
         <div id="anotacoes">
             <div id="escrever-anotacao"></div>
 
@@ -169,7 +61,7 @@
 
             <form action="../tratar_acao.php" method="POST">
                 <div>
-                    <label for="itutulo">Título: </label>
+                    <label for="ititulo">Título: </label>
                     <input type="text" name="titulo" id="ititulo" required>
                 </div>
                 
@@ -181,9 +73,6 @@
                 <input type="submit" value="Adicionar">
             </form>
 
-            <button id="adicionar" onclick="popup()" title="Adicionar anotação">
-                +
-            </button>
 
             <?php 
                 require("../db.php");
@@ -191,7 +80,6 @@
                 $sql = "SELECT titulo, anotacao, id_anotacao FROM anotacoes WHERE id_usuario = ?";
                 
                 $stmt = $PDO -> prepare($sql);
-
                 $stmt -> execute([$_SESSION["usuario_id"]]);
 
                 $linha = $stmt -> fetchAll(PDO::FETCH_ASSOC);
@@ -199,39 +87,70 @@
                 foreach($linha as $dado):
             ?>
 
-            <?php if ($qtd_anotacoes > 0): ?>
+                <?php if ($qtd_anotacoes > 0): ?>
 
-                <div id="mostrar-anotacao">
-                    <h3><?php echo $dado["titulo"]?></h3>
-                    <button id="remover" onclick="window.location.href='../tratar_acao.php?linha=<?= $dado['id_anotacao'] ?>'">
-                        <img src="../imagens/remover.svg" alt="Remover anotação" title="Remover anotação">
-                    </button>
-                </div>
+                    <div id="mostrar-anotacao">
+                        <h3 class="exibir-anotacao" title="Clique para exibir a anotação"><?php echo $dado["titulo"]?></h3>
+                        <button id="remover" onclick="window.location.href='../tratar_acao.php?linha=<?= $dado['id_anotacao'] ?>'">
+                            <img src="../imagens/remover.svg" alt="Remover anotação" title="Remover anotação">
+                        </button>
+                    </div>
 
-            <?php endif; ?>
-
+                <?php endif; ?>
             <?php endforeach; ?>
-           
         </div>
 
+        <div id="popup-anotacao">
+            <div id="fundo-meio">
+                <h3 id="titulo">Título</h3>
+                <p id="data">Data de criação: </p>
+                <div>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis fuga temporibus explicabo inventore esse nemo, ipsum assumenda corporis nobis. Nam soluta aut hic, recusandae eaque earum nulla eligendi. Totam, quia.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque excepturi totam sint ipsa, rem quae temporibus numquam expedita odit odio! Doloremque rerum, obcaecati ex aliquid temporibus a voluptates deserunt in.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius cupiditate quis optio quod voluptates mollitia explicabo neque quas est dolore! Aperiam corrupti deserunt adipisci quaerat ratione, possimus atque voluptates consequatur.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut assumenda, sapiente ducimus molestiae voluptate veritatis ad accusantium et nisi dolore est totam ipsam corporis saepe quia aspernatur eaque, vel ab.
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, recusandae voluptas quod consequatur suscipit a illo culpa necessitatibus soluta? Cumque officia rerum iusto minus nam accusamus! Ullam cupiditate incidunt suscipit!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta natus, nobis voluptatem nam fugit esse neque. Maiores autem odit quos magnam dolores, dolorem saepe eaque architecto? Fugiat mollitia consequatur sequi?
+                </div>
+                
+            </div>
+        </div>
     </div>
     
 </body>
 
 <script>
+
+
+    //document.getElementById('exibir-anotacao').addEventListener('click', function() {
+    //    document.getElementById('popup-anotacao').style.display = 'block';
+    // });
+    
+    var elementos = document.getElementsByClassName('exibir-anotacao');
+
+    for (let i = 0; i < elementos.length;i++) {
+
+        elementos[i].addEventListener('click', function() {
+            document.getElementById('popup-anotacao').style.display = 'block';
+        });
+    }
+
+    document.getElementById('popup-anotacao').addEventListener('click', function() {
+        this.style.display = 'none';
+    });
+
     document.getElementById("escrever-anotacao").addEventListener('click', function() {
         
         let form = document.getElementsByTagName('form')[0];
-        let fundoCinza = document.getElementById("escrever-anotacao");
 
-        fundoCinza.style.display = 'none';
+        this.style.display = 'none';
         form.style.display = 'none';
     });
     
     function popup() {
         let popup = document.getElementById('escrever-anotacao');
         let form = document.getElementsByTagName('form')[0];
-        console.log("funcionando");
+        
         popup.style.display = 'block';
         form.style.display = 'flex';
     }
